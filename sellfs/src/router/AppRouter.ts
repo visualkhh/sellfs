993dbc739ac283a0966024e2314e072b1f53e5de
김현하(visual) <118410962+jaranda-visual@users.noreply.github.com>
2023-09-24 20:54:16 +0900

    test

---
+++ sellfs/src/router/AppRouter.ts
@@ -0,0 +1,12 @@
+import {Sim} from 'simple-boot-core/decorators/SimDecorator';
+import {Router} from 'simple-boot-core/decorators/route/Router';
+
+@Sim
+@Router({
+  // route: {
+  //   '/test': ApiTest
+  // }
+})
+export class AppRouter {
+
+}
